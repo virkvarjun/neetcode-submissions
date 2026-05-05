fd9708de319eda23e8bc9729c54db7b89b329a71
Arjun Virk <virkvarjun@gmail.com>
2026-05-05 10:07:41 -0400

    Add: invert-a-binary-tree - submission-5

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-5.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr) {
+            return nullptr;
+        }
+        std::swap(root->left, root->right);
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    }
+};
